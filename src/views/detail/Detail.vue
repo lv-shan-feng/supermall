894d1c2bf1baa2decdd29a32540939a9918a755a
lv_shan_feng <2500818105@qq.com>
2020-11-27 22:08:16 +0800

    '详情页导航'

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,17 +1,37 @@
 <template>
-  <div>{{iid}}</div>
+  <div id="detail">
+    <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :top-images="topImages"></detail-swiper>
+  </div>
 </template>
 
 <script>
+  import DetailNavBar from "./childComps/DetailNavBar";
+  import DetailSwiper from "./childComps/DetailSwiper";
+
+  import {getDetail} from "network/detail";
+
   export default {
     name: "Detail",
+    components: {
+      DetailNavBar,
+      DetailSwiper
+    },
     data() {
       return {
-        iid: null
+        iid: null,
+        topImages: []
       }
     },
     created() {
+      // 1.保存传入的额iid
       this.iid = this.$route.params.iid;
+
+      // 2.根据iid请求详情数据
+      getDetail(this.iid).then(res => {
+        // 1.获取顶部的图片轮播数据
+        this.topImages = res.result.itemInfo.topImages;
+      })
     }
   }
 </script>
